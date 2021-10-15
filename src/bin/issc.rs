bb1204bbb820b0480101ff322949934c0d0463c3
Rob Tsuk <rob@tsuk.com>
2021-10-15 07:35:59 -0700

    Fix date for schedule request

--- src/bin/issc.rs
+++ src/bin/issc.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Error, Result};
+use chrono::{DateTime, Utc};
 use games_today::{issc::InSeasonCupResults, Schedule};
 
 #[allow(unused)]
@@ -6,7 +7,13 @@ const URL: &'static str = "https://podcast.sportsnet.ca/31-thoughts/the-in-seaso
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
-    let uri = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=2021-10-12&endDate=2021-10-15&gameType=R";
+    let date_time_now: DateTime<Utc> = Utc::now();
+    let date = date_time_now.date();
+    let uri = format!(
+            "https://statsapi.web.nhl.com/api/v1/schedule?startDate=2021-10-12&endDate={}&gameType=R",
+            date.format("%F")
+        );
+        dbg!(&uri);
     let string: String = surf::get(uri)
         .recv_string()
         .await
