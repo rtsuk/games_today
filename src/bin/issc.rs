30b1eab6b53509220759651b3d118bb0daa99320
Rob Tsuk <rob@tsuk.com>
2021-10-15 07:26:23 -0700

    Move issc calc into library

--- src/bin/issc.rs
+++ src/bin/issc.rs
@@ -1,123 +1,12 @@
-use anyhow::{anyhow, Error, Result};
-use chrono::{Date, Utc};
-use deunicode::deunicode;
-use games_today::{teams::*, Schedule, Team};
-use inflector::Inflector;
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-
-const TEAMS_TEXT: &str = include_str!("../../data/teams.json");
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-struct AllTeams {
-    teams: Vec<Team>,
-}
-
-#[allow(unused)]
-const CAROLYN_TEAMS: [usize; 8] = [
-    VEGAS_GOLDEN_KNIGHTS_ID,
-    NEW_YORK_RANGERS_ID,
-    PHILADELPHIA_FLYERS_ID,
-    SEATTLE_KRAKEN_ID,
-    CHICAGO_BLACKHAWKS_ID,
-    PITTSBURGH_PENGUINS_ID,
-    COLUMBUS_BLUE_JACKETS_ID,
-    ANAHEIM_DUCKS_ID,
-];
-
-#[allow(unused)]
-const DAVID_TEAMS: [usize; 8] = [
-    COLORADO_AVALANCHE_ID,
-    VANCOUVER_CANUCKS_ID,
-    FLORIDA_PANTHERS_ID,
-    WASHINGTON_CAPITALS_ID,
-    TORONTO_MAPLE_LEAFS_ID,
-    OTTAWA_SENATORS_ID,
-    MONTREAL_CANADIENS_ID,
-    BUFFALO_SABRES_ID,
-];
-
-#[allow(unused)]
-const JEFF_TEAMS: [usize; 8] = [
-    TAMPA_BAY_LIGHTNING_ID,
-    CAROLINA_HURRICANES_ID,
-    DETROIT_RED_WINGS_ID,
-    EDMONTON_OILERS_ID,
-    MINNESOTA_WILD_ID,
-    ST_LOUIS_BLUES_ID,
-    LOS_ANGELES_KINGS_ID,
-    ARIZONA_COYOTES_ID,
-];
-
-#[allow(unused)]
-const ELIOTTE_TEAMS: [usize; 8] = [
-    NEW_YORK_ISLANDERS_ID,
-    WINNIPEG_JETS_ID,
-    DALLAS_STARS_ID,
-    BOSTON_BRUINS_ID,
-    CALGARY_FLAMES_ID,
-    NEW_JERSEY_DEVILS_ID,
-    NASHVILLE_PREDATORS_ID,
-    SAN_JOSE_SHARKS_ID,
-];
+use anyhow::{Error, Result};
+use games_today::issc::InSeasonCupResults;
 
 #[allow(unused)]
 const URL: &'static str = "https://podcast.sportsnet.ca/31-thoughts/the-in-season-stanley-cup/";
 
 #[async_std::main]
 async fn main() -> Result<(), Error> {
-    if false {
-        let all_teams: AllTeams = serde_json::from_str(&TEAMS_TEXT).expect("from_str");
-        for team in all_teams.teams {
-            println!(
-                "pub const {}_ID : usize = {};",
-                deunicode(&team.name).to_screaming_snake_case(),
-                team.id
-            )
-        }
-    }
-    let uri = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=2021-10-12&endDate=2021-10-13&gameType=R";
-    //    let uri = "https://statsapi.web.nhl.com/api/v1/schedule?season=20172018&gameType=R";
-    let string: String = surf::get(uri)
-        .recv_string()
-        .await
-        .map_err(|e| anyhow!("e: {}", e))?;
-    let schedule: Schedule = serde_json::from_str(&string)?;
-    let mut last_transfer_date: Option<Date<Utc>> = None;
-    let mut current_in_season = 14;
-    let mut current_in_season_name = "".to_string();
-    let mut days_with_cup: HashMap<String, usize> = HashMap::new();
-    for date in schedule.dates {
-        println!("{}", date.date.format("%x"));
-        for game in date.games {
-            if game.is_finished() && game.is_regular_season() {
-                if game.has_competitor(current_in_season) {
-                    if let Some((new_holder, new_holder_name)) =
-                        game.check_for_handoff(current_in_season)
-                    {
-                        println!("{} takes the cup", new_holder_name);
-                        if let Some(last_transfer_date) = last_transfer_date {
-                            let days = game.game_date.date() - last_transfer_date;
-                            let current_days =
-                                days_with_cup.entry(new_holder_name.to_owned()).or_insert(0);
-                            *current_days += days.num_days() as usize;
-                        }
-                        last_transfer_date = Some(game.game_date.date());
-                        current_in_season = new_holder;
-                        current_in_season_name = new_holder_name;
-                    }
-                }
-            }
-        }
-    }
-
-    if let Some(last_transfer_date) = last_transfer_date {
-        let days = Utc::today() - last_transfer_date;
-        let current_days = days_with_cup.entry(current_in_season_name).or_insert(0);
-        *current_days += days.num_days() as usize;
-    }
-
-    dbg!(days_with_cup);
+    let insc = InSeasonCupResults::new().await?;
+    dbg!(&insc);
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,11 @@ use serde::{Deserialize, Serialize};
 mod pages;
 
 pub mod teams {
+    use crate::Team;
+    use deunicode::deunicode;
+    use inflector::Inflector;
+    use serde::{Deserialize, Serialize};
+
     pub const ANAHEIM_DUCKS_ID: usize = 24;
     pub const ARIZONA_COYOTES_ID: usize = 53;
     pub const BOSTON_BRUINS_ID: usize = 6;
@@ -36,6 +41,63 @@ pub mod teams {
     pub const VEGAS_GOLDEN_KNIGHTS_ID: usize = 54;
     pub const WASHINGTON_CAPITALS_ID: usize = 15;
     pub const WINNIPEG_JETS_ID: usize = 52;
+
+    pub fn team_name(team_id: usize) -> &'static str {
+        match team_id {
+            ANAHEIM_DUCKS_ID => "Anaheim Ducks",
+            ARIZONA_COYOTES_ID => "Arizona Coyotes",
+            BOSTON_BRUINS_ID => "Boston Bruins",
+            BUFFALO_SABRES_ID => "Buffalo Sabres",
+            CALGARY_FLAMES_ID => "Calgary Flames",
+            CAROLINA_HURRICANES_ID => "Carolina Hurricanes",
+            CHICAGO_BLACKHAWKS_ID => "Chicago Blackhawks",
+            COLORADO_AVALANCHE_ID => "Colorado Avalanche",
+            COLUMBUS_BLUE_JACKETS_ID => "Columbus Blue Jackets",
+            DALLAS_STARS_ID => "Dallas Stars",
+            DETROIT_RED_WINGS_ID => "Detroit Red Wings",
+            EDMONTON_OILERS_ID => "Edmonton Oilers",
+            FLORIDA_PANTHERS_ID => "Florida Panthers",
+            LOS_ANGELES_KINGS_ID => "Los Angeles Kings",
+            MINNESOTA_WILD_ID => "Minnesota Wild",
+            MONTREAL_CANADIENS_ID => "Montréal Canadiens",
+            NASHVILLE_PREDATORS_ID => "Nashville Predators",
+            NEW_JERSEY_DEVILS_ID => "New Jersey Devils",
+            NEW_YORK_ISLANDERS_ID => "New York Islanders",
+            NEW_YORK_RANGERS_ID => "New York Rangers",
+            OTTAWA_SENATORS_ID => "Ottawa Senators",
+            PHILADELPHIA_FLYERS_ID => "Philadelphia Flyers",
+            PITTSBURGH_PENGUINS_ID => "Pittsburgh Penguins",
+            SAN_JOSE_SHARKS_ID => "San Jose Sharks",
+            SEATTLE_KRAKEN_ID => "Seattle Kraken",
+            ST_LOUIS_BLUES_ID => "St. Louis Blues",
+            TAMPA_BAY_LIGHTNING_ID => "Tampa Bay Lightning",
+            TORONTO_MAPLE_LEAFS_ID => "Toronto Maple Leafs",
+            VANCOUVER_CANUCKS_ID => "Vancouver Canucks",
+            VEGAS_GOLDEN_KNIGHTS_ID => "Vegas Golden Knights",
+            WASHINGTON_CAPITALS_ID => "Washington Capitals",
+            WINNIPEG_JETS_ID => "Winnipeg Jets",
+            _ => "",
+        }
+    }
+
+    const TEAMS_TEXT: &str = include_str!("../data/teams.json");
+
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+    #[serde(rename_all = "camelCase")]
+    struct AllTeams {
+        teams: Vec<Team>,
+    }
+
+    pub fn get_teams() {
+        let all_teams: AllTeams = serde_json::from_str(&TEAMS_TEXT).expect("from_str");
+        for team in all_teams.teams {
+            println!(
+                "{}_ID => \"{}\",",
+                deunicode(&team.name).to_screaming_snake_case(),
+                team.name
+            )
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -124,25 +186,19 @@ impl Game {
         self.teams.away.team.id == competitor || self.teams.home.team.id == competitor
     }
 
-    pub fn winner(&self) -> (usize, String) {
+    pub fn winner(&self) -> usize {
         if self.teams.away.score > self.teams.home.score {
-            (
-                self.teams.away.team.id,
-                self.teams.away.team.name.to_string(),
-            )
+            self.teams.away.team.id
         } else {
-            (
-                self.teams.home.team.id,
-                self.teams.home.team.name.to_string(),
-            )
+            self.teams.home.team.id
         }
     }
 
-    pub fn check_for_handoff(&self, competitor: usize) -> Option<(usize, String)> {
+    pub fn check_for_handoff(&self, competitor: usize) -> Option<usize> {
         if self.has_competitor(competitor) {
-            let (winner, name) = self.winner();
+            let winner = self.winner();
             if winner != competitor {
-                Some((winner, name))
+                Some(winner)
             } else {
                 None
             }
@@ -175,12 +231,152 @@ impl Default for Schedule {
     }
 }
 
+pub mod issc {
+    use crate::{teams::*, Schedule};
+    use anyhow::{anyhow, Error};
+    use chrono::{Date, Utc};
+    use std::collections::HashMap;
+
+    const CAROLYN_TEAMS: [usize; 8] = [
+        VEGAS_GOLDEN_KNIGHTS_ID,
+        NEW_YORK_RANGERS_ID,
+        PHILADELPHIA_FLYERS_ID,
+        SEATTLE_KRAKEN_ID,
+        CHICAGO_BLACKHAWKS_ID,
+        PITTSBURGH_PENGUINS_ID,
+        COLUMBUS_BLUE_JACKETS_ID,
+        ANAHEIM_DUCKS_ID,
+    ];
+
+    const DAVID_TEAMS: [usize; 8] = [
+        COLORADO_AVALANCHE_ID,
+        VANCOUVER_CANUCKS_ID,
+        FLORIDA_PANTHERS_ID,
+        WASHINGTON_CAPITALS_ID,
+        TORONTO_MAPLE_LEAFS_ID,
+        OTTAWA_SENATORS_ID,
+        MONTREAL_CANADIENS_ID,
+        BUFFALO_SABRES_ID,
+    ];
+
+    const JEFF_TEAMS: [usize; 8] = [
+        TAMPA_BAY_LIGHTNING_ID,
+        CAROLINA_HURRICANES_ID,
+        DETROIT_RED_WINGS_ID,
+        EDMONTON_OILERS_ID,
+        MINNESOTA_WILD_ID,
+        ST_LOUIS_BLUES_ID,
+        LOS_ANGELES_KINGS_ID,
+        ARIZONA_COYOTES_ID,
+    ];
+
+    const ELIOTTE_TEAMS: [usize; 8] = [
+        NEW_YORK_ISLANDERS_ID,
+        WINNIPEG_JETS_ID,
+        DALLAS_STARS_ID,
+        BOSTON_BRUINS_ID,
+        CALGARY_FLAMES_ID,
+        NEW_JERSEY_DEVILS_ID,
+        NASHVILLE_PREDATORS_ID,
+        SAN_JOSE_SHARKS_ID,
+    ];
+
+    #[derive(Debug)]
+    pub struct Handoff {
+        date: Date<Utc>,
+        from: usize,
+        to: usize,
+    }
+
+    #[derive(Default, Debug)]
+    pub struct InSeasonCupResults {
+        pub team_stats: HashMap<usize, usize>,
+        pub standings: HashMap<String, usize>,
+        pub handoffs: Vec<Handoff>,
+    }
+
+    impl InSeasonCupResults {
+        pub async fn new() -> Result<Self, Error> {
+            let uri = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=2021-10-12&endDate=2021-10-15&gameType=R";
+            //    let uri = "https://statsapi.web.nhl.com/api/v1/schedule?season=20172018&gameType=R";
+            let string: String = surf::get(uri)
+                .recv_string()
+                .await
+                .map_err(|e| anyhow!("e: {}", e))?;
+            let schedule: Schedule = serde_json::from_str(&string)?;
+            let mut last_transfer_date: Option<Date<Utc>> = None;
+            let mut current_in_season = 14;
+            let mut days_with_cup: HashMap<usize, usize> = HashMap::new();
+            let mut handoffs = Vec::new();
+            for date in schedule.dates {
+                for game in date.games {
+                    if game.is_finished() && game.is_regular_season() {
+                        if game.has_competitor(current_in_season) {
+                            if let Some(new_holder) = game.check_for_handoff(current_in_season) {
+                                if let Some(last_transfer_date) = last_transfer_date {
+                                    let days = game.game_date.date() - last_transfer_date;
+                                    let current_days =
+                                        days_with_cup.entry(current_in_season).or_insert(0);
+                                    *current_days += days.num_days() as usize;
+                                    handoffs.push(Handoff {
+                                        date: game.game_date.date(),
+                                        from: current_in_season,
+                                        to: new_holder,
+                                    });
+                                } else {
+                                    handoffs.push(Handoff {
+                                        date: game.game_date.date(),
+                                        from: 14,
+                                        to: new_holder,
+                                    });
+                                }
+                                last_transfer_date = Some(game.game_date.date());
+                                current_in_season = new_holder;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if let Some(last_transfer_date) = last_transfer_date {
+                let days = Utc::today() - last_transfer_date;
+                let current_days = days_with_cup.entry(current_in_season).or_insert(0);
+                *current_days += days.num_days() as usize;
+            }
+
+            let players = [
+                ("Carolyn", CAROLYN_TEAMS),
+                ("David", DAVID_TEAMS),
+                ("Jeff", JEFF_TEAMS),
+                ("Elliotte", ELIOTTE_TEAMS),
+            ];
+
+            let mut standings = HashMap::new();
+            for (player, teams) in &players {
+                let days: usize = teams
+                    .iter()
+                    .map(|team_id| days_with_cup.get(team_id).unwrap_or(&0))
+                    .sum();
+                standings.insert(player.to_string(), days);
+            }
+
+            Ok(Self {
+                team_stats: days_with_cup,
+                standings,
+                handoffs,
+                ..Default::default()
+            })
+        }
+    }
+}
+
 mod web {
     use wasm_bindgen::prelude::*;
     use yew::prelude::*;
 
     use crate::pages::GamesToday;
 
+    #[allow(unused)]
     #[wasm_bindgen(start)]
     pub fn run_app() {
         wasm_logger::init(wasm_logger::Config::default());
